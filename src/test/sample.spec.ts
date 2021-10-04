68f06444d50aa26d5f0d84ca7a19ee8563adb0fb
juntae <rlawnsxo131@gmail.com>
2021-10-05 01:05:25 +0900

    .

--- src/test/sample.spec.ts
+++ src/test/sample.spec.ts
@@ -1,18 +1,8 @@
 import { expect } from 'chai';
-import { Database } from '../db';
-import initializeEnvironment from '../lib/initializeEnvironment';
 
-initializeEnvironment();
-const database = new Database();
-
-describe('sample test', () => {
+describe('Hello World Test', () => {
   it('hello world', () => {
     const world = 'world';
     expect(world).equal('world');
-  });
-
-  it('database connection test', async () => {
-    const connection = await database.getConnection();
-    expect(connection).not.null;
   });
 });
