0ac693579f048ba95b9e670354d53f9810afe30c
juntae <rlawnsxo131@gmail.com>
2021-10-23 23:54:40 +0900

    .

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { Database } from './datastore';
 import initializeEnvironment from './lib/initializeEnvironment';
 import Application from './application';
+import { Connection } from 'typeorm';
 
 initializeEnvironment();
 const database = new Database();
 const application = new Application();
 
-database.getConnection().then(async () => {
+database.getConnection().then(async (_: Connection) => {
   await application.setup();
   await application.start();
 });
