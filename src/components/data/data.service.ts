5c8b7d349e0dabbf016d34434be8208fd093b388
juntae <rlawnsxo131@gmail.com>
2021-10-24 03:45:40 +0900

    change service file shape

--- src/components/data/data.service.ts
+++ src/components/data/data.service.ts
@@ -1,11 +1,9 @@
 import { getCustomRepository } from 'typeorm';
 import { DataRepository } from '.';
 
-function findById(id: number) {
-  const dataRepo = getCustomRepository(DataRepository);
-  return dataRepo.findOne(id);
-}
-
 export default {
-  findById,
+  findById(id: number) {
+    const dataRepo = getCustomRepository(DataRepository);
+    return dataRepo.findOne(id);
+  },
 };
--- src/components/user/user.service.ts
+++ src/components/user/user.service.ts
@@ -1,11 +1,9 @@
 import { getCustomRepository } from 'typeorm';
 import { UserRepository } from '.';
 
-function findById(id: number) {
-  const userRepo = getCustomRepository(UserRepository);
-  return userRepo.findOne({ id });
-}
-
 export default {
-  findById,
+  findById(id: number) {
+    const userRepo = getCustomRepository(UserRepository);
+    return userRepo.findOne({ id });
+  },
 };
