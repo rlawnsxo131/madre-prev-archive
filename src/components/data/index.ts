993b0d7b1c760d16fb4a24b31ff4390c84a683fe
juntae <rlawnsxo131@gmail.com>
2021-11-03 01:29:00 +0900

    .

--- src/components/data/index.ts
+++ src/components/data/index.ts
@@ -1,5 +1,5 @@
 export { default as Data } from './data.entity';
 export { default as DataRepository } from './data.repository';
+export { default as dataError } from './data.error';
 export { default as dataService } from './data.service';
 export { default as dataGraphQL } from './data.graphql';
-export { default as dataError } from './data.error';
--- src/components/user/index.ts
+++ src/components/user/index.ts
@@ -1,5 +1,5 @@
 export { default as User } from './user.entity';
 export { default as UserRepository } from './user.repository';
+export { default as userError } from './user.error';
 export { default as userService } from './user.service';
 export { default as userGraphQL } from './user.graphql';
-export { default as userError } from './user.error';
