2152bb1f4381b0fdd0bdbe50ea47685441926bd8
juntae <rlawnsxo131@gmail.com>
2021-12-19 23:44:15 +0900

    minor code changes related to error handling

--- src/lib/CustomError.ts
+++ src/lib/CustomError.ts
@@ -1,10 +1,16 @@
 /**
  * let's think a little more
  */
+
+interface CustomErrorParams {
+  statusCode: number;
+  name: string;
+  message: string;
+}
 export default class CustomError extends Error {
   statusCode: number;
   name: string;
-  constructor(statusCode: number, name: string, message: string) {
+  constructor({ statusCode, name, message }: CustomErrorParams) {
     super(message);
     this.statusCode = statusCode;
     this.name = name;
