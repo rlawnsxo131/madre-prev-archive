41c8f5e787fe13b3537dc1713e85a9b34e9a3eb8
juntae <rlawnsxo131@gmail.com>
2021-12-20 22:01:55 +0900

    add validation service test cases

---
+++ src/components/data/test/data.validation.service.test.ts
@@ -0,0 +1,54 @@
+import 'jest';
+import { ApolloError } from 'apollo-server-core';
+import { dataValidationService } from '..';
+import { errorService } from '../../error';
+import { CreateDataParams } from '../interface/data.interface';
+
+describe('dataValidationService Test', () => {
+  test('getDataParamsValidation', async () => {
+    const id = '';
+    const code = 'BAD_USER_INPUT';
+    try {
+      const validation = dataValidationService.getDataParamsValidation(id);
+      errorService.throwApolloError({
+        resolver: () => !validation,
+        message: 'validation test',
+        code,
+        params: { id },
+      });
+    } catch (e) {
+      const error = e as ApolloError;
+      expect(error.extensions.code).toBe(code);
+      expect(error.extensions.id).toBe(id);
+    }
+  });
+
+  test('createDataParamsValidation', async () => {
+    const createDataParams: CreateDataParams = {
+      user_id: '',
+      file_url: '',
+      is_public: false,
+      title: '',
+      description: '',
+    };
+    const code = 'BAD_USER_INPUT';
+    try {
+      const validation =
+        dataValidationService.createDataParamsValidation(createDataParams);
+      errorService.throwApolloError({
+        resolver: () => !validation,
+        message: 'createData mutation validation error',
+        code,
+        params: createDataParams,
+      });
+    } catch (e) {
+      const error = e as ApolloError;
+      expect(error.extensions.code).toBe(code);
+      expect(error.extensions.user_id).toBe(createDataParams.user_id);
+      expect(error.extensions.file_url).toBe(createDataParams.file_url);
+      expect(error.extensions.is_public).toBe(createDataParams.is_public);
+      expect(error.extensions.title).toBe(createDataParams.title);
+      expect(error.extensions.description).toBe(createDataParams.description);
+    }
+  });
+});
--- src/components/error/test/error.service.test.ts
+++ src/components/error/test/error.service.test.ts
@@ -5,17 +5,17 @@ import { ApolloError } from 'apollo-server-core';
 describe('errorService Test', () => {
   test('throwApolloError: type NOT_FOUND and id to 1', async () => {
     const id = 1;
-    const errorType = 'NOT_FOUND';
+    const code = 'NOT_FOUND';
     try {
       errorService.throwApolloError({
         resolver: () => true,
         message: 'Not Found Data',
-        code: errorType,
+        code,
         params: { id },
       });
     } catch (e) {
       const error = e as ApolloError;
-      expect(error.extensions.code).toBe(errorType);
+      expect(error.extensions.code).toBe(code);
       expect(error.extensions.id).toBe(id);
     }
   });
