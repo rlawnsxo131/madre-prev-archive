43f3dcb3b1f65610e77c3eb97f8eadcbf085de51
juntae <rlawnsxo131@gmail.com>
2021-12-26 01:02:12 +0900

    change the name of the file that directly exports the function

--- src/application/Apollo/apollo.schema.ts
+++ src/application/Apollo/apollo.schema.ts
@@ -1,8 +1,8 @@
 import { gql } from 'apollo-server-core';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import merge from 'lodash.merge';
-import { DataGraphQL } from '../../components/data';
-import { UserGraphQL } from '../../components/user';
+import { userGraphQL } from '../../components/user';
+import { dataGraphQL } from '../../components/data';
 
 const typeDef = gql`
   scalar Date
@@ -22,11 +22,11 @@ const resolvers = {
 };
 
 const schema = makeExecutableSchema({
-  typeDefs: [typeDef, UserGraphQL.userTypeDef, DataGraphQL.dataTypeDef],
+  typeDefs: [typeDef, userGraphQL.userTypeDef, dataGraphQL.dataTypeDef],
   resolvers: merge(
     resolvers,
-    UserGraphQL.userResolvers,
-    DataGraphQL.dataResolvers,
+    userGraphQL.userResolvers,
+    dataGraphQL.dataResolvers,
   ),
 });
 
