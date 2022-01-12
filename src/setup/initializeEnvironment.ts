df7768c94709fb9586cfe753e6c1d7d93bd31264
juntae <rlawnsxo131@gmail.com>
2022-01-12 23:13:31 +0900

    modify logger export file name

--- src/setup/initializeEnvironment.ts
+++ src/setup/initializeEnvironment.ts
@@ -1,10 +1,8 @@
 import { config } from 'dotenv';
 import { resolve } from 'path';
 import { environmentFilename } from '../constants';
-import logger from '../lib/logger/logger';
 
 export default function initializeEnvironment() {
-  logger.info('run initalizeEnvironment');
   return config({
     path: resolve(process.cwd(), environmentFilename),
   });
