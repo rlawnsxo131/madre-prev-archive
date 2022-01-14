7d5e18e4790e6460743c630de981fe2ea470cea8
juntae <rlawnsxo131@gmail.com>
2022-01-14 13:35:08 +0900

    CHANGE: the function that was written after the validator file inside lib was divided is moved to the index.ts file.

--- src/lib/validator/index.ts
+++ src/lib/validator/index.ts
@@ -1,2 +1,40 @@
-export { default as apolloValidator } from './apolloValidator';
-export { default as fastifyValidator } from './fastifyValidator';
+import ApolloCustomError from '../ApolloCustomError';
+import FastifyCustomError from '../FastifyCustomError';
+import { validateId, validateObject } from './validator';
+import { Validator, ValidatorHelper } from './validator.type';
+
+/**
+ * apollo
+ */
+const apolloHelper: ValidatorHelper = (error, params) => {
+  if (!error) return;
+  throw new ApolloCustomError({
+    message: error.message,
+    code: 'BAD_REQUEST',
+    extensions: params,
+  });
+};
+
+export const apolloValidator: Validator = {
+  validateId: (id) => validateId(id)(apolloHelper),
+  validateObject: (schema, params = {}) =>
+    validateObject(schema, params)(apolloHelper),
+};
+
+/**
+ * fastify
+ */
+const fastifyHelper: ValidatorHelper = (error, params) => {
+  if (!error) return;
+  throw new FastifyCustomError({
+    message: `params: ${JSON.stringify(params)}\n${error.message}`,
+    name: 'BadRequestError',
+    statusCode: 400,
+  });
+};
+
+export const fastifyValidator: Validator = {
+  validateId: (id) => validateId(id)(fastifyHelper),
+  validateObject: (schema, params = {}) =>
+    validateObject(schema, params)(fastifyHelper),
+};
