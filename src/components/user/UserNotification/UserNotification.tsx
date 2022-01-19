26b099ede99cb07c6737aa04ccef63c692fb5a81
juntae <rlawnsxo131@gmail.com>
2022-01-20 03:06:31 +0900

    Add: add UserNotification file

---
+++ src/components/user/UserNotification/UserNotification.tsx
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+interface UserNotificationProps {}
+
+function UserNotification(props: UserNotificationProps) {
+  return <div css={block}></div>;
+}
+
+const block = css``;
+
+export default UserNotification;
