c640eac7a0a4f80b8f8e67f320bedc4f18306bbb
juntae <rlawnsxo131@gmail.com>
2022-02-24 02:44:20 +0900

    Refactor: refactor GetGoogleProfile func

--- lib/google.go
+++ lib/google.go
@@ -10,36 +10,64 @@ const (
 	googlePeopleURL = "https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses,photos"
 )
 
-func GetGoogleProfile(accessToken string) (p interface{}, err error) {
-	req, err := http.NewRequest("GET", googlePeopleURL, nil)
-	req.Header.Add("Authorization", "Bearer "+accessToken)
+func GetGoogleProfile(accessToken string) *profile {
+	req := createRequest(accessToken)
+	res := excuteGoogleProfileRequest(req)
+	mapData := convertToMap(res)
+	googleProfileStruct := generateGoogleProfileStruct(mapData)
 
+	return googleProfileStruct
+}
+
+func createRequest(accessToken string) *http.Request {
+	req, err := http.NewRequest("GET", googlePeopleURL, nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Authorization", "Bearer "+accessToken)
+	return req
+}
+
+func excuteGoogleProfileRequest(req *http.Request) *http.Response {
 	client := &http.Client{}
 	res, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
 	defer res.Body.Close()
+	return res
+}
 
-	profile := make(map[string]interface{})
-	if err := json.NewDecoder(res.Body).Decode(&profile); err != nil {
+func convertToMap(res *http.Response) map[string]interface{} {
+	mapData := make(map[string]interface{})
+	if err := json.NewDecoder(res.Body).Decode(&mapData); err != nil {
 		panic(err)
 	}
+	return mapData
+}
+
+type profile struct {
+	socialId    string
+	email       string
+	photo       string
+	displayName string
+}
+
+func generateGoogleProfileStruct(mapData map[string]interface{}) *profile {
+	resourceName := mapData["resourceName"]
+	emailAddresses := mapData["emailAddresses"]
+	photos := mapData["photos"]
+	displayName := mapData["names"]
 
 	// socialId: data.resourceName?.replace('people/', '') ?? '',
 	// email: data.emailAddresses?.[0].value ?? '',
 	// photo: data.photos?.[0].url ?? null,
 	// displayName: data.names?.[0].displayName?.split(' (')[0] ?? '',
 
-	resourceName := profile["resourceName"]
-	emailAddresses := profile["emailAddresses"]
-	photos := profile["photos"]
-	displayName := profile["names"]
-
 	log.Println(resourceName)
 	log.Println(emailAddresses)
 	log.Println(photos)
 	log.Println(displayName)
 
-	return "", err
+	return &profile{}
 }
