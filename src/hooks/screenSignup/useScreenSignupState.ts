511a861936215d6c089d4d37782b2d3f9ee2acbb
juntae <rlawnsxo131@gmail.com>
2022-04-02 02:32:39 +0900

    Add: remove cache

--- src/hooks/screenSignup/useScreenSignupState.ts
+++
@@ -1,6 +0,0 @@
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store';
-
-export default function useScreenSignUpState() {
-  return useSelector((state: RootState) => state.screenSignUp);
-}
