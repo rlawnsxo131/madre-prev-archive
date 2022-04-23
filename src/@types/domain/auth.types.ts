987ab842d0826b47a6f8a58c353e04dcae2f577e
juntae <rlawnsxo131@gmail.com>
2022-04-24 04:20:32 +0900

    Add: Change of type declaration by schema change of server side

--- src/@types/domain/auth.types.ts
+++ src/@types/domain/auth.types.ts
@@ -1,7 +1,7 @@
 export type SocialAccountProvider = 'GOOGLE';
 export interface SocialAccount {
-  id: number;
-  user_id: number;
+  id: string;
+  user_id: string;
   provider: SocialAccountProvider;
   social_id: string;
   created_at: string;
--- src/@types/domain/user.types.ts
+++ src/@types/domain/user.types.ts
@@ -1,5 +1,5 @@
 export interface User {
-  id: number;
+  id: string;
   email: string;
   origin_name?: string;
   display_name: string;
