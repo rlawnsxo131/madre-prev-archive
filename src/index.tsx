1e21110b92f5cc8ee0d93232e7a8ef0b10cfce61
john <john@cleaninglab@co.kr>
2022-05-03 12:20:55 +0900

    Fix: fix typo

--- src/index.tsx
+++ src/index.tsx
@@ -21,11 +21,11 @@ const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  // <StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  // </StrictMode>,
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>,
 );
