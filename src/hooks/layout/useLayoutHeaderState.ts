9bdd0249a9f3476d7f3d3afc9308c6a319854482
juntae <rlawnsxo131@gmail.com>
2022-05-27 22:30:13 +0900

    Remove: remove layout store and hooks

--- src/hooks/layout/useLayoutHeaderState.ts
+++
@@ -1,6 +0,0 @@
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store';
-
-export default function useHeaderState() {
-  return useSelector((state: RootState) => state.layout.header);
-}
