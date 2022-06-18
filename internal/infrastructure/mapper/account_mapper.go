58a9a6d24e3de0fe335d5df5af7431b2eafc6cfb
juntae <rlawnsxo131@gmail.com>
2022-06-18 15:36:04 +0900

    Refactor: mapper 에 필요없는 pointer 제거

--- internal/infrastructure/mapper/account_mapper.go
+++ internal/infrastructure/mapper/account_mapper.go
@@ -4,7 +4,7 @@ import "github.com/rlawnsxo131/madre-server-v3/internal/domain/account"
 
 type AccountMapper struct{}
 
-func (am *AccountMapper) ToUserEntity(u *account.User) *account.User {
+func (am AccountMapper) ToUserEntity(u *account.User) *account.User {
 	return &account.User{
 		ID:         u.ID,
 		Email:      u.Email,
@@ -16,7 +16,7 @@ func (am *AccountMapper) ToUserEntity(u *account.User) *account.User {
 	}
 }
 
-func (am *AccountMapper) ToUserModel(u *account.User) *account.User {
+func (am AccountMapper) ToUserModel(u *account.User) *account.User {
 	return &account.User{
 		Email:      u.Email,
 		OriginName: u.OriginName,
@@ -26,7 +26,7 @@ func (am *AccountMapper) ToUserModel(u *account.User) *account.User {
 	}
 }
 
-func (am *AccountMapper) ToSocialAccountEntity(sa *account.SocialAccount) *account.SocialAccount {
+func (am AccountMapper) ToSocialAccountEntity(sa *account.SocialAccount) *account.SocialAccount {
 	return &account.SocialAccount{
 		ID:        sa.ID,
 		UserID:    sa.UserID,
@@ -37,7 +37,7 @@ func (am *AccountMapper) ToSocialAccountEntity(sa *account.SocialAccount) *accou
 	}
 }
 
-func (am *AccountMapper) ToSocialAccountModel(sa *account.SocialAccount) *account.SocialAccount {
+func (am AccountMapper) ToSocialAccountModel(sa *account.SocialAccount) *account.SocialAccount {
 	return &account.SocialAccount{
 		UserID:   sa.UserID,
 		Provider: sa.Provider,
