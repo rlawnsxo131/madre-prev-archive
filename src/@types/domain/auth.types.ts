37664cfb965ecad3105e8941f180d7e64c98c00c
juntae <rlawnsxo131@gmail.com>
2022-07-03 04:06:07 +0900

    Add: account interface 추가

--- src/@types/domain/auth.types.ts
+++ src/@types/domain/auth.types.ts
@@ -1,4 +1,16 @@
 export type SocialAccountProvider = 'GOOGLE';
+
+export interface Account {
+  user_id: number;
+  username: string;
+  email: string;
+  photo_url: string;
+  social_id: string;
+  provider: SocialAccountProvider;
+  created_at: string;
+  updated_at: string;
+}
+
 export interface SocialAccount {
   id: string;
   user_id: string;
