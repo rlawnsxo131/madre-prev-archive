d925b2f186801892b5ef1d052064c02187720746
juntae <rlawnsxo131@gmail.com>
2022-07-28 21:31:43 +0900

    Change: 일단 pages 디렉터리 구조만 index.ts 로 export 하도록 바꿔서 실험중

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,1 +1,11 @@
-export { default } from './NotFoundPage';
+import { css } from '@emotion/react';
+
+interface NotFoundPageProps {}
+
+function NotFoundPage(props: NotFoundPageProps) {
+  return <div css={block}>not found</div>;
+}
+
+const block = css``;
+
+export default NotFoundPage;
