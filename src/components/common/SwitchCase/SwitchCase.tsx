fb747f8d7280e65c394fed9328f0c93aee507ddf
juntae <rlawnsxo131@gmail.com>
2023-06-18 02:47:25 +0900

    refactor(SwitchCase.tsx): 불필요한 export 제거

--- src/components/common/SwitchCase/SwitchCase.tsx
+++ src/components/common/SwitchCase/SwitchCase.tsx
@@ -1,4 +1,4 @@
-export type SwitchCaseProps<Case extends string> = {
+type SwitchCaseProps<Case extends string> = {
   caseBy: Partial<Record<Case, JSX.Element | null>>;
   value?: Case;
   defaultComponent?: JSX.Element | null;
