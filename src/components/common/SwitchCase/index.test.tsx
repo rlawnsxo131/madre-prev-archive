69ab42f6beed43432556cdf3f42831e55be5742e
juntae <rlawnsxo131@gmail.com>
2023-06-18 03:03:49 +0900

    test: desc 수정

--- src/components/common/SwitchCase/index.test.tsx
+++ src/components/common/SwitchCase/index.test.tsx
@@ -16,20 +16,20 @@ function prepare(key: string) {
 }
 
 describe('<SwitchCase />는', () => {
-  it(`'a' 라는 값을 주면 'Hello A' 가 렌더링 된다.`, () => {
+  it("'a' 라는 값을 주면 'Hello A' 가 렌더링 된다.", () => {
     const controls = prepare('a');
 
     expect(controls.getByText('Hello A')).toBeInTheDocument();
   });
 
-  it(`'a' 라는 값을 주면 'Hello B' 는 렌더링되면 안됩니다.`, () => {
+  it("'a' 라는 값을 주면 'Hello B' 는 렌더링되면 안됩니다.", () => {
     const controls = prepare('a');
 
     expect(controls.getByText('Hello A')).toBeInTheDocument();
     expect(controls.queryByText('Hello B')).not.toBeInTheDocument();
   });
 
-  it(`만족하는 값이 없다면 'Default' 가 렌더링 되어야 합니다.`, () => {
+  it("만족하는 값이 없다면 'Default' 가 렌더링 되어야 합니다.", () => {
     const controls = prepare('cc');
 
     expect(controls.queryByText('Hello A')).not.toBeInTheDocument();
