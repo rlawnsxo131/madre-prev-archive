e95aec035cd484da27168f111cfa39fc98c8942a
rlawnsxo131 <rlawnsxo131@gmail.com>
2023-09-28 01:35:51 +0900

    fix: 인덱스 추가

--- domain/persistence/init.sql
+++ domain/persistence/init.sql
@@ -20,5 +20,6 @@ CREATE TABLE IF NOT EXISTS madre.user_social_account(
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `ix_user_id` (`user_id`),
   UNIQUE KEY `ix_social_id_provider` (`social_id`, `provider`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
